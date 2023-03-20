76830de153fedde682759e304c05f4bc2c440615
ShahriarHossainDev <shahriar.hossain.dev@gmail.com>
2023-03-20 12:36:18 +0600

    update

--- Calendar View/Serecns/Calendar/View/CalendarViewController.swift
+++ Calendar View/Serecns/Calendar/View/CalendarViewController.swift
@@ -1,20 +1,18 @@
 //
-//  ViewController.swift
+//  CalendarViewController.swift
 //  Calendar View
 //
 //  Created by Shishir_Mac on 20/3/23.
 //
 
 import UIKit
+import CalendarKit
 
-class ViewController: UIViewController {
+class CalendarViewController: DayViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Calender"
         // Do any additional setup after loading the view.
-        view.backgroundColor = .red
     }
-
-
 }
-
